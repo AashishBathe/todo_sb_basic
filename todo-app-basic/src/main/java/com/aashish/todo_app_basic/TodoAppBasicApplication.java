b3febdb8b98099bc2ea2d0a0d6d782b76538dced
CoderAashishXD <98446454+CoderAashishXD@users.noreply.github.com>
2024-06-15 11:23:31 +0530

    First commit

---
+++ todo-app-basic/src/main/java/com/aashish/todo_app_basic/TodoAppBasicApplication.java
@@ -0,0 +1,13 @@
+package com.aashish.todo_app_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppBasicApplication.class, args);
+	}
+
+}
---
+++ todo-app-basic/src/test/java/com/aashish/todo_app_basic/TodoAppBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashish.todo_app_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
